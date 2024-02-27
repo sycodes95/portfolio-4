937ca779f6595086aa0147e6701b8c1c2e8adc92
kevin kim <sycodes95@gmail.com>
2024-02-27 01:33:28 -0800

    added placeholder content in active quest component

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,6 @@ function App() {
       {/* <Nav />
        */}
 
-    
-
     </main>
   )
 }
--- src/components/about/bio.tsx
+++ src/components/about/bio.tsx
@@ -1,38 +1,32 @@
 import PictureFrame from "../pictureFrame";
 import MyPic from "../../assets/pics/selfie.jpg"
+import BioRow from "./bioRow";
+import ActiveQuest from "./activeQuest";
 
 export default function Bio () {
+
+  const bioData = [
+    { title: 'NAME', value: 'Kevin Kim'},
+    { title: 'ALIAS', value: 'Untalented Web Dev'},
+
+    { title: 'ORIGIN', value: 'Los Angeles, CA'},
+    { title: 'OCCUPATION', value: 'Fullstack Developer'},
+  ];
   return (
     <div className="w-full h-fit flex gap-4">
       <div className="grid md:grid-cols-3 gap-12 w-full">
         <PictureFrame src={MyPic} />
 
-        <div className="flex flex-col gap-8 w-full">
-          <div className="flex flex-col gap-1">
-            <span className="text-white text-sm border-b border-white/5 pb-2">NAME</span>
-            <span className="text-zinc-500 text-md ">
-              <em>Kevin Kim</em>
-            </span>
-          </div>
+        <div className="grid grid-rows-3 gap-4 w-full h-full">
 
-          <div className="flex flex-col gap-1">
-            <span className="text-white text-sm">ORIGIN</span>
-            <span className="text-zinc-500 text-md ">
-              <em>Los Angeles, CA</em>
-            </span>
-          </div>
-
-          <div className="flex flex-col gap-1">
-            <span className="text-white text-sm">OCCUPATION</span>
-            <span className="text-zinc-500 text-md ">
-              <em>Fullstack Developer</em>
-            </span>
-          </div>
+          {
+          bioData.map((data) => (
+            <BioRow title={data.title} value={data.value}/>
+          ))
+          }
+         
         </div>
-
-        <div className="h-full w-full flex flex-col border-l border-red-500">
-          <span className="h-fit w-full bg-red-500 text-sm p-1 pl-2 font-semibold">ACTIVE QUEST</span>
-        </div>
+        <ActiveQuest />
       </div>
 
       
